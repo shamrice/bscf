0c542fdd5735c106455c27dec922b163d6afe435
Erik <shamrice@gmail.com>
2026-09-23 16:28:43 -0400

    #10 : Reworked how template renderer can be configured so that both file and text can be displayed together instead of one or the other

--- lib/BSCF/Template/TemplateRenderer.pm
+++ lib/BSCF/Template/TemplateRenderer.pm
@@ -15,6 +15,11 @@ use constant {
     OFFLINE_TEMPLATE => 'offline_screen',
     CONNECT_TEMPLATE => 'connect_screen',
     MAX_CONNECTIONS_TEMPLATE => 'max_connections',
+
+    RENDER_MODE_FILE => 'FILE',
+    RENDER_MODE_MSG => 'MSG',
+    RENDER_MODE_ALL => 'ALL',
+    RENDER_MODE_NONE => 'NONE',
 };
 
 our @EXPORT_OK = qw(
@@ -31,9 +36,18 @@ sub new {
     my $config = BSCF::Configuration::Config->new(package => __PACKAGE__);
     my @valid_templates = (BUSY_TEMPLATE, OFFLINE_TEMPLATE, CONNECT_TEMPLATE);
 
+    my $render_mode = uc($config->get('render_mode', RENDER_MODE_FILE));
+
+    my @valid_render_modes = (RENDER_MODE_FILE, RENDER_MODE_MSG, RENDER_MODE_ALL, RENDER_MODE_NONE);
+    if (!grep(/^\Q$render_mode\E$/, @valid_render_modes)) {
+        $log->error("Invalid render mode configured: $render_mode :: Valid modes: [" . join(',', @valid_render_modes) . "] :: Defaulting to mode: " . RENDER_MODE_FILE);
+        $render_mode = RENDER_MODE_FILE;
+    }
+
     my $self = {
         config => $config,
         log => $log,
+        render_mode => $render_mode,
         valid_templates => \@valid_templates,
     };
 
@@ -48,6 +62,9 @@ sub _config {
     return shift->{config};
 }
 
+sub _render_mode {
+    return shift->{render_mode};
+}
 
 sub _valid_templates {
     return shift->{valid_templates};
@@ -59,30 +76,41 @@ sub render {
 
     $self->_log->info("Rendering template: $template_type");
 
+    return if ($self->_render_mode eq RENDER_MODE_NONE);
+
     if (!grep(/^\Q$template_type\E$/, $self->_valid_templates->@*)) {
         $self->_log->fatal("Invalid template type: $template_type :: Sending client error message");
         $client->send("Sorry, an error has occurred.");
         return;
     }
 
-    my $file = $self->_config->get($template_type . '_file', '');
     my $msg = $self->_config->get($template_type . '_msg', 'Sorry! An error has occurred');
 
-    if (! -e $file) {
+    if ($self->_render_mode eq RENDER_MODE_MSG) {
         $client->send($msg);
         return;
     }
 
-    open(my $FH, "<", $file) or do {
-        $self->_log->fatal("Missing $file ! :: $!");
-        $client->send($msg);
-        return;
-    };
+    my $is_send_msg = $self->_render_mode eq RENDER_MODE_ALL ? 1 : 0;
+    my $file = $self->_config->get($template_type . '_file', '');
+    my @data;
 
-    my @data = <$FH>;
-    close($FH);
+    if (! -e $file) {
+        $self->_log->error("Render mode set to: " . $self->_render_mode . " but template file was not found for template: $template_type! :: Sending message only");
+        $is_send_msg = 1;
+    } else {
+
+        open(my $FH, "<", $file) or do {
+            $self->_log->fatal("Missing or error reading: $file ! :: $!");
+            $is_send_msg = 1;
+        };
+
+        @data = <$FH>;
+        close($FH);
+    }
 
     $client->send($_) foreach (@data);
+    $client->send($msg) if ($is_send_msg);
 
     return;
 }
